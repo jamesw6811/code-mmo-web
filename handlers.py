f85a39c3186f58f5956b5eb651ef22e868ac6baf
James White <jamesw6811@gmail.com>
2015-02-22 16:25:58 -0500

    Remove instance and log when errors happen

--- handlers.py
+++ handlers.py
@@ -179,6 +179,12 @@ class LoadMonitorHandler(webapp2.RequestHandler):
     if loadresp[LoadInfo.STATUS] == LoadInfo.STATUS_LOADING:
       newresp = ComputeEngineController().checkResponse(loadresp[LoadInfo.LAST_RESP])
       logging.info('Loading latest resp:'+str(newresp))
+      if(newresp.has_key("error")){
+        logging.warn('Error loading server, removing from LoadInfo:'+str(newresp['error']))
+        LoadInfo.RemoveInstance(name)
+      } else {
+        logging.info('No errors, still loading...');
+      }
     self.response.out.write(json.dumps(loadresp))
 
 
